2782badb49ed20d4b0ee12bcdc67dc99f7c7d24f
Daniel Bortot <daniel.bortot@gmail.com>
2024-04-30 01:42:14 -0400

    Modulo auth, dtos, entity User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    DatabaseModule
+    DatabaseModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @ApiProperty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(11)
+    @MaxLength(11)
+    phone: string;
+
+    @ApiProperty()
+    @IsString()
+    password: number;
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(30)
+    name: string
+}
