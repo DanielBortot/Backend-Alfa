2daf854a3466d4010ed77c2b5807e6bffe654b17
TheRedZone97 <66092857+TheRedZone97@users.noreply.github.com>
2024-05-05 21:30:04 -0400

    se creo el CRUD del modulo de notificaciones y unos añadidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { VideoModule } from './video/video.module';
 import { AuthModule } from './auth/auth.module';
+import { NotifyModule } from './notify/notify.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
     VideoModule,
-    AuthModule
+    AuthModule,
+    NotifyModule,
   ],
   controllers: [],
   providers: [],
